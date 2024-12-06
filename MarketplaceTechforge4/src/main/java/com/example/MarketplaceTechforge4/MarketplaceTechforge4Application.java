786f75e97b714916fab49b1898cfbec6e74e9ec6
vini-garcia <advviniciusgarcia@gmail.com>
2024-12-06 19:15:37 -0300

    Commit inicial

---
+++ MarketplaceTechforge4/src/main/java/com/example/MarketplaceTechforge4/MarketplaceTechforge4Application.java
@@ -0,0 +1,10 @@
+package com.example.MarketplaceTechforge4;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceTechforge4Application {
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceTechforge4Application.class, args);
+	}
+}
---
+++ MarketplaceTechforge4/src/test/java/com/example/MarketplaceTechforge4/MarketplaceTechforge4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MarketplaceTechforge4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceTechforge4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
